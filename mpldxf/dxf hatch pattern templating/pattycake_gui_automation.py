30e280708143e9da16d3549178ad94aba01f16a6
subsurfaceiodev <subsurfaceiodev@gmail.com>
2024-01-01 22:18:48 -0500

    empty

--- mpldxf/dxf hatch pattern templating/pattycake_gui_automation.py
+++ mpldxf/dxf hatch pattern templating/pattycake_gui_automation.py
@@ -6,9 +6,6 @@ from tkinter import Tk
 import numpy as np
 from fractions import Fraction
 
-gw.getWindowsWithTitle('- Pattycake')[0].maximize()
-
-
 def get_df(canvas_width, canvas_dimension_is):
     # click on advanced settings
     pg.click(1043, 215)
@@ -36,29 +33,46 @@ def get_df(canvas_width, canvas_dimension_is):
     return df
 
 
-# pattycake won't accept canvas width or height values less than 0.125!
-canvas_dimensions = np.arange(0.13, 4 + 0.01, 0.01)
-canvas_dimension_is = 'height'
-# canvas_dimensions = [0.125, 0.14, 0.2, 0.3]
-dfs = {}
-for canvas_dimension in canvas_dimensions:
-    canvas_dimension = round(canvas_dimension, 3)
+def step1():
+    gw.getWindowsWithTitle('- Pattycake')[0].maximize()
+    # pattycake won't accept canvas width or height values less than 0.125!
+    canvas_dimensions = np.arange(0.13, 4 + 0.01, 0.01)
+    canvas_dimension_is = 'width'
+    # canvas_dimensions = [0.125, 0.14, 0.2, 0.3]
+    dfs = {}
+    for canvas_dimension in canvas_dimensions:
+        canvas_dimension = round(canvas_dimension, 3)
 
-    try:
-        df = get_df(canvas_dimension, canvas_dimension_is=canvas_dimension_is)
-    except pd.errors.EmptyDataError as e:
-        df = pd.DataFrame([{'angle': np.nan}])
+        try:
+            df = get_df(canvas_dimension, canvas_dimension_is=canvas_dimension_is)
+        except pd.errors.EmptyDataError as e:
+            df = pd.DataFrame([{'angle': np.nan}])
 
-    dfs[canvas_dimension] = df
-dfs = pd.concat(dfs, names=[f'canvas_{canvas_dimension_is}']).droplevel(1)
-dfs['d'] = dfs['dash'] + dfs['space'].abs()
-angle = dfs['angle'].iloc[0]
-angle_rad = np.deg2rad(angle)
-sin_angle = np.sin(angle_rad)
-tan_angle = np.tan(angle_rad)
-angle_fraction = Fraction(tan_angle).limit_denominator(max_denominator=100)
-dfs['d_y'] = sin_angle * dfs['d']
-dfs['d_x'] = dfs['d_y'] / tan_angle
-dfs['multiplier'] = dfs['d_x'] / angle_fraction.denominator
-dfs.to_excel(f'canvas_{canvas_dimension_is}s_pats.xlsx')
-print(dfs)
+        dfs[canvas_dimension] = df
+    dfs = pd.concat(dfs, names=[f'canvas_{canvas_dimension_is}']).droplevel(1)
+    dfs['d'] = dfs['dash'] + dfs['space'].abs()
+    angle = dfs['angle'].iloc[0]
+    angle_rad = np.deg2rad(angle)
+    sin_angle = np.sin(angle_rad)
+    tan_angle = np.tan(angle_rad)
+    angle_fraction = Fraction(tan_angle).limit_denominator(max_denominator=100)
+    dfs['d_y'] = sin_angle * dfs['d']
+    dfs['d_x'] = dfs['d_y'] / tan_angle
+    dfs['multiplier'] = dfs['d_x'] / angle_fraction.denominator
+    dfs[['d_y', 'd_x', 'multiplier']] = dfs[['d_y', 'd_x', 'multiplier']].round(8)
+    dfs.to_excel(f'{canvas_dimension_is}=variable.xlsx')
+    print(dfs)
+
+
+def concat_results():
+    from glob import glob
+    dfs = []
+    for fname in glob('pattycake/*.xlsx'):
+        df = pd.read_excel(fname, index_col=[0, 1])
+        dfs.append(df)
+    dfs = pd.concat(dfs)
+    dfs.to_excel('pattycake_concat.xlsx', merge_cells=False)
+
+
+# step1()
+concat_results()
